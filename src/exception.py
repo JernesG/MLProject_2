9741c433dcdaedc7460ecf914c85fb895554f1b1
JernesG <georgenjernes@gmail.com>
2025-07-23 19:01:51 +0530

    dataset

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -7,12 +8,13 @@ def error_message_detail(error, error_detail:sys):
     error_messsage = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name,line_no,str(error)
     )
-    return Exception
+    return error_messsage
 
-    class CustomException(Exception):
-        def __init__(self, error_message, error_detail:sys):
-            super.__init__(error_message)
-            self.error_message = error_messsage_detail(error_messsage,error_detail=error_detail)
-            
-        def __str__(self):
-            return self.error_message
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
